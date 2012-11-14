11cba11f79bc5f1c98b324126a01e4723ce69249
nordy <nordy@b551ab73-3a01-0010-9fae-47f737f7a8aa>
2012-11-14 22:15:45 +0000

    VOLK datei für EC hinzugefügt

---
+++ src/oc/wh30k/armies/VOLKEmperorsChildren.java
@@ -0,0 +1,60 @@
+package oc.wh30k.armies;
+
+
+
+
+import oc.BuildaHQ;
+import oc.BuildaVater;
+import oc.ChooserGruppe;
+
+public class VOLKEmperorsChildren extends BuildaVater {
+
+	public VOLKEmperorsChildren() {
+		reflectionKennung = "EC";
+		reflectionKennungAllies ="EC";
+
+		String[] HQeinträge = {"", "Legion Praetor", "Legion Centurion", "", "Chaplain", "Forge lord", "Librarian", "Master of signal", "Moritat", "Primus medicae", "Siege breaker", "Vigilator", "", "Legion Command Squad"};
+		String[] Eliteeinträge = {"", "Palatin Blade Squad", "Legion Veteran Tactical Squad", "Legion Destroyer Squad", "Legion Terminator Squad", "Techmarine Covenant", "Apothecarion Detachment", "Legion Dreadnought Talon", "Contemptor Dreadnought Talon", "Legion Rapier Weapons Battery"};
+		String[] Standardeinträge = {"", "Legion Tactical Squad", "Legion Assault Squad", "Legion Breacher Siege Squad", "Legion Tactical Support Squad", "Legion Reconnaissance Squad"};
+		String[] Sturmeinträge = {"", "Legion Seeker Squad", "Legion Outrider Squad", "Legion Attack Bike Squadron", "Legion Jetbike Sky Hunter Squadron", "Legion Land Speeder Squadron", "Legion Storm Eagle Assault Gunship"};
+		String[] Unterstützungeinträge = {"", "Legion Heavy Support Squad", "Legion Predator Strike Armour Squadron", "Legion Land Raider Battle Squadron", "Legion Artillery Tank Squadron", "Legion Vindicator", "Legion Spartan Assault Tank", "Legion Caestus Assault Ram"};
+		String[] Befestigungen = {"", "Aegis-Verteidigungslinie", "Himmelsschild-Landeplattform", "Imperiale Bastion", "Festung der Erlösung"};
+
+		adden(new ChooserGruppe(reflectionKennung, cnt, 0, 1, HQeinträge));
+		cnt += CHOOSERGRUPPEN_X_ABSTAND;
+		adden(new ChooserGruppe(reflectionKennung, cnt, 0, 2, Eliteeinträge));
+		cnt += CHOOSERGRUPPEN_X_ABSTAND;
+		adden(new ChooserGruppe(reflectionKennung, cnt, 0, 3, Standardeinträge));
+		cnt += CHOOSERGRUPPEN_X_ABSTAND;
+		adden(new ChooserGruppe(reflectionKennung, cnt, 0, 4, Sturmeinträge));
+		cnt += CHOOSERGRUPPEN_X_ABSTAND;
+		adden(new ChooserGruppe(reflectionKennung, cnt, 0, 5, Unterstützungeinträge));
+		cnt += CHOOSERGRUPPEN_X_ABSTAND;
+		adden(new ChooserGruppe("FO", cnt, 0, 6, Befestigungen));
+		cnt += 480;
+
+		nameDerArtDerArmee = "Emperors Children";
+		nameDerArtDerArmeeDekliniert = "Emperors Children";
+		textAreaUeberschrift = "Emperors Children";
+		/*
+		BuildaHQ.getChooserGruppe(2).addSpezialAuswahl("Sanguinische Garde");
+
+		noAllies.clear();
+		noAllies.add("BA");
+		noAllies.add("CD");
+		noAllies.add("CM");
+		noAllies.add("DE");
+		noAllies.add("OR");
+		noAllies.add("TY");
+		noAllies.add("RH");
+		noAllies.add("DM");
+		noAllies.add("TL");
+		*/
+		complete();
+	}
+
+	@Override
+	protected void finalize() {
+		System.out.println("Object gelöscht in " + nameDerArtDerArmee + "Builda !!");
+	}
+}
