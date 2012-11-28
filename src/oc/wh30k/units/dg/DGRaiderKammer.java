da5703c34b0ebf42bfccb82079b72b678708933c
Krimdar <Krimdar@b551ab73-3a01-0010-9fae-47f737f7a8aa>
2012-11-28 14:28:14 +0000

    #11 Fehlende Unterstützungseinheiten

---
+++ src/oc/wh30k/units/dg/DGRaiderKammer.java
@@ -0,0 +1,74 @@
+package oc.wh30k.units.dg;
+
+import oc.BuildaHQ;
+import oc.OptionsEinzelUpgrade;
+import oc.OptionsEinzelZaehler;
+import oc.OptionsGruppeEintrag;
+import oc.OptionsUpgradeGruppe;
+import oc.OptionsZaehlerGruppe;
+import oc.RuestkammerVater;
+
+public class DGRaiderKammer  extends RuestkammerVater {
+
+	OptionsUpgradeGruppe o1, o2, o3, o4, o5;
+
+	public DGRaiderKammer () {
+            grundkosten = 0;
+	}
+
+	public void initButtons(boolean... defaults) {
+
+
+		seperator();
+		
+		ogE.addElement(new OptionsGruppeEintrag("Land Raider Phobos", 250));
+		ogE.addElement(new OptionsGruppeEintrag("Land Raider Proteus", 200));
+		ogE.addElement(new OptionsGruppeEintrag("Land Raider Achilles", 300));
+		add(o1 = new OptionsUpgradeGruppe(ID, randAbstand, cnt, "", ogE));
+		
+		seperator();
+		ogE.addElement(new OptionsGruppeEintrag("Hunter-killer missile", 10));
+		ogE.addElement(new OptionsGruppeEintrag("Dozer blade", 5));
+		ogE.addElement(new OptionsGruppeEintrag("Auxiliary drive", 10));
+		ogE.addElement(new OptionsGruppeEintrag("Extra armour", 10));
+		ogE.addElement(new OptionsGruppeEintrag("Armoured ceramite", 20));
+		add(o2 = new OptionsUpgradeGruppe(ID, randAbstand, cnt, "", ogE, 5));	
+		
+		if (o1.isSelected(0)) {
+		ogE.addElement(new OptionsGruppeEintrag("Frag assault launchers", 10));
+		add(o3 = new OptionsUpgradeGruppe(ID, randAbstand, cnt, "", ogE));
+		}
+		else if (o1.isSelected(1)) {
+		ogE.addElement(new OptionsGruppeEintrag("Explorator Augury Web", 50));
+		add(o3 = new OptionsUpgradeGruppe(ID, randAbstand, cnt, "", ogE));
+		}
+		
+		seperator();
+		ogE.addElement(new OptionsGruppeEintrag("Combi-bolter", 5));
+		ogE.addElement(new OptionsGruppeEintrag("Combi-weapon", 10));
+		ogE.addElement(new OptionsGruppeEintrag("Heavy flamer", 10));
+		ogE.addElement(new OptionsGruppeEintrag("Heavy bolter", 15));
+		ogE.addElement(new OptionsGruppeEintrag("Havoc launcher", 15));
+		ogE.addElement(new OptionsGruppeEintrag("Multi-melta", 20));
+		add(o4 = new OptionsUpgradeGruppe(ID, randAbstand, cnt, "", ogE));
+		
+		if(o1.isSelected(1)) {
+		seperator();
+		ogE.addElement(new OptionsGruppeEintrag("Twin heavy bolter", "Twin-linked heavy bolter", 20));
+		ogE.addElement(new OptionsGruppeEintrag("Twin heavy flamer", "Twin-linked heavy flamer", 20));
+		add(o5 = new OptionsUpgradeGruppe(ID, randAbstand, cnt, "", ogE));
+		}
+		
+		seperator();
+		
+        sizeSetzen();
+	}
+
+	@Override
+	public void refreshen() {
+			if (!o1.isSelected()) o1.setSelected(0, true);
+			
+		
+	}
+        
+}
