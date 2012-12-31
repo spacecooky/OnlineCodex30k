f63d92ac38060356add625651b45d40a02f3fb05
Krimdar <Krimdar@b551ab73-3a01-0010-9fae-47f737f7a8aa>
2012-12-31 15:02:08 +0000

    Command Squad Bugfix

---
+++ trunk/src/oc/wh30k/armies/VOLKOrdoReductor.java
@@ -0,0 +1,62 @@
+package oc.wh30k.armies;
+
+
+
+
+import oc.BuildaHQ;
+import oc.BuildaVater;
+import oc.ChooserGruppe;
+
+public class VOLKOrdoReductor extends BuildaVater {
+
+	public VOLKOrdoReductor() {
+		reflectionKennung = "OR";
+		reflectionKennungAllies ="OR";
+
+		String[] HQeinträge = {"", "Magos Reductor Calleb Decima"};
+		String[] Eliteeinträge = {""};
+		String[] Standardeinträge = {"", "Mechanicum Thallax Cohort"};
+		String[] Sturmeinträge = {""};
+		String[] Unterstützungeinträge = {""};
+		String[] Befestigungen = {"", "Aegis-Verteidigungslinie", "Himmelsschild-Landeplattform", "Imperiale Bastion", "Festung der Erlösung"};
+		String[] LordsofWar = {"", "Imperial Avenger Strike Fighter", "Warhound Scout Titan", "Reaver Battle Titan"};
+
+		adden(new ChooserGruppe(reflectionKennung, cnt, 0, 1, HQeinträge));
+		cnt += CHOOSERGRUPPEN_X_ABSTAND;
+		adden(new ChooserGruppe(reflectionKennung, cnt, 0, 2, Eliteeinträge));
+		cnt += CHOOSERGRUPPEN_X_ABSTAND;
+		adden(new ChooserGruppe(reflectionKennung, cnt, 0, 3, Standardeinträge));
+		cnt += CHOOSERGRUPPEN_X_ABSTAND;
+		adden(new ChooserGruppe(reflectionKennung, cnt, 0, 4, Sturmeinträge));
+		cnt += CHOOSERGRUPPEN_X_ABSTAND;
+		adden(new ChooserGruppe(reflectionKennung, cnt, 0, 5, Unterstützungeinträge));
+		cnt += CHOOSERGRUPPEN_X_ABSTAND;
+		adden(new ChooserGruppe("FO", cnt, 0, 6, Befestigungen));
+		cnt += CHOOSERGRUPPEN_X_ABSTAND;
+		adden(new ChooserGruppe(reflectionKennung, cnt, 0, 7, LordsofWar));
+		cnt += 480;
+
+		nameDerArtDerArmee = "Ordo Reductor";
+		nameDerArtDerArmeeDekliniert = "Ordo Reductor";
+		textAreaUeberschrift = "Ordo Reductor";
+		
+		/*
+		noAllies.clear();
+		noAllies.add("BA");
+		noAllies.add("CD");
+		noAllies.add("CM");
+		noAllies.add("DE");
+		noAllies.add("OR");
+		noAllies.add("TY");
+		noAllies.add("RH");
+		noAllies.add("DM");
+		noAllies.add("TL");
+		*/
+		complete();
+	}
+
+	@Override
+	protected void finalize() {
+		System.out.println("Object gelöscht in " + nameDerArtDerArmee + "Builda !!");
+	}
+}
