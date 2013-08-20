9dfd6c3b96cd37c2ae6a9f5562c8797b15683964
Krimdar <Krimdar@b551ab73-3a01-0010-9fae-47f737f7a8aa>
2013-08-20 08:28:36 +0000

    #13.08.20 Ordo Reductor überarbeitet

---
+++ src/oc/wh30k/units/or/ORArtilleryKammer.java
@@ -0,0 +1,93 @@
+package oc.wh30k.units.or;
+
+import javax.swing.ImageIcon;
+
+import oc.BuildaHQ;
+import oc.OnlineCodex;
+import oc.OptionsEinzelUpgrade;
+import oc.OptionsGruppeEintrag;
+import oc.OptionsPanelSwitcher;
+import oc.OptionsUpgradeGruppe;
+import oc.OptionsVater;
+import oc.RuestkammerVater;
+
+public class ORArtilleryKammer extends RuestkammerVater {
+
+	OptionsUpgradeGruppe typ;
+	OptionsUpgradeGruppe rhinoWeapons;
+	OptionsPanelSwitcher switcher;
+	OptionsUpgradeGruppe o1;
+	OptionsUpgradeGruppe o2;
+	
+	public ORArtilleryKammer() {
+		grundkosten = 0;
+	}
+
+	@Override
+	public void initButtons(boolean... defaults) {
+
+		
+		ogE.addElement(new OptionsGruppeEintrag("Legion Basilisk", 140));
+		ogE.addElement(new OptionsGruppeEintrag("Legion Medusa", 155));
+		ogE.addElement(new OptionsGruppeEintrag("Legion Whirlwind", 75));
+		add(typ = new OptionsUpgradeGruppe(ID, randAbstand, cnt, "", ogE));
+
+		seperator();
+
+		OptionsVater[] basilisk = new OptionsVater[]{
+			new OptionsEinzelUpgrade(ID, randAbstand, cnt, "", "Hunter-killer missile", 10),
+			new OptionsEinzelUpgrade(ID, randAbstand, cnt, "", "Dozer blade", 5),
+			new OptionsEinzelUpgrade(ID, randAbstand, cnt, "", "Auxiliary drive", 10),
+			new OptionsEinzelUpgrade(ID, randAbstand, cnt, "", "Extra armour", 10),	
+			new OptionsEinzelUpgrade(ID, randAbstand, cnt, "", "Twin-linked bolter", 5),
+		};
+		
+		OptionsVater[] medusa = new OptionsVater[]{
+			new OptionsEinzelUpgrade(ID, randAbstand, cnt, "", "Hunter-killer missile", 10),
+			new OptionsEinzelUpgrade(ID, randAbstand, cnt, "", "Dozer blade", 5),
+			new OptionsEinzelUpgrade(ID, randAbstand, cnt, "", "Auxiliary drive", 10),
+			new OptionsEinzelUpgrade(ID, randAbstand, cnt, "", "Extra armour", 10),	
+			new OptionsEinzelUpgrade(ID, randAbstand, cnt, "", "Twin-linked bolter", 5),
+		};
+		
+		OptionsVater[] whirlwind = new OptionsVater[]{
+			new OptionsEinzelUpgrade(ID, randAbstand, cnt, "", "Hunter-killer missile", 10),
+			new OptionsEinzelUpgrade(ID, randAbstand, cnt, "", "Dozer blade", 5),
+			new OptionsEinzelUpgrade(ID, randAbstand, cnt, "", "Auxiliary drive", 10),
+			new OptionsEinzelUpgrade(ID, randAbstand, cnt, "", "Extra armour", 10),	
+			new OptionsEinzelUpgrade(ID, randAbstand, cnt, "", "Twin-linked bolter", 5),
+			new OptionsEinzelUpgrade(ID, randAbstand, cnt, "", "Hyperios missiles", "Hyperios air-defence missiles", 0),
+		};
+		
+		add(switcher = new OptionsPanelSwitcher(randAbstand, cnt, basilisk, medusa, whirlwind));
+			
+		ogE.addElement(new OptionsGruppeEintrag("Phosphex shells", 0));
+		add(o1 = new OptionsUpgradeGruppe(ID, randAbstand, cnt, "", ogE));
+
+		sizeSetzen();
+	}
+
+	@Override
+	public void refreshen() {
+		if (!typ.isSelected()) {
+			typ.setSelected(0, true);
+		}
+		
+		if (typ.isSelected("Legion Basilisk")) {
+			switcher.forceSwitchPanel(0);
+		} else if (typ.isSelected("Legion Medusa")) {
+			switcher.forceSwitchPanel(1);
+		} else {
+			switcher.forceSwitchPanel(2);
+		}
+		
+		int phosphex = BuildaHQ.getCountFromInformationVector("ORSiege");
+		o1.setAktiv (typ.isSelected(1) && phosphex >0);
+		o1.getPanel().setLocation(
+			(int) switcher.getPanel().getLocation().getX(),
+			(int) switcher.getPanel().getLocation().getY() + switcher.getPanel().getSize().height + 8
+		);
+				
+	}
+	
+}
---
+++ src/oc/wh30k/units/or/ORTechmarineKammer.java
@@ -0,0 +1,87 @@
+package oc.wh30k.units.or;
+
+import oc.BuildaHQ;
+import oc.OptionsEinzelUpgrade;
+import oc.OptionsEinzelZaehler;
+import oc.OptionsGruppeEintrag;
+import oc.OptionsUpgradeGruppe;
+import oc.OptionsZaehlerGruppe;
+import oc.RuestkammerStarter;
+import oc.RuestkammerVater;
+
+public class ORTechmarineKammer  extends RuestkammerVater {
+
+	OptionsUpgradeGruppe o1, o2, o3, o4;
+	OptionsZaehlerGruppe o5, o6;
+	RuestkammerStarter rkTransport;
+
+	public ORTechmarineKammer () {
+            grundkosten = 45;
+	}
+
+	public void initButtons(boolean... defaults) {
+
+
+		seperator();
+
+		ogE.addElement(new OptionsGruppeEintrag("Power axe", 0));
+		ogE.addElement(new OptionsGruppeEintrag("Thunder hammer", 15));;
+		add(o1 = new OptionsUpgradeGruppe(ID, randAbstand, cnt, "", ogE));
+		
+		seperator();
+
+		ogE.addElement(new OptionsGruppeEintrag("Nuncio-vox", 10));
+		ogE.addElement(new OptionsGruppeEintrag("Augury scanner", 5));
+		ogE.addElement(new OptionsGruppeEintrag("Master-crafted bolter", 5));
+		ogE.addElement(new OptionsGruppeEintrag("Combi-flamer", 10));
+		ogE.addElement(new OptionsGruppeEintrag("Combi-melta", 10));
+		ogE.addElement(new OptionsGruppeEintrag("Combi-plasma", 10));
+		ogE.addElement(new OptionsGruppeEintrag("Volkite charger", 10));
+		ogE.addElement(new OptionsGruppeEintrag("Plasma pistol", 15));
+		ogE.addElement(new OptionsGruppeEintrag("Graviton gun", 15));;
+		add(o2 = new OptionsUpgradeGruppe(ID, randAbstand, cnt, "", ogE));
+		
+		seperator();
+		
+		ogE.addElement(new OptionsGruppeEintrag("Melta bombs", 5));
+		ogE.addElement(new OptionsGruppeEintrag("Rad grenades", 10));
+		ogE.addElement(new OptionsGruppeEintrag("Cyber familiar", 15));
+		add(o3 = new OptionsUpgradeGruppe(ID, randAbstand, cnt, "", ogE, 3));
+		
+		seperator();
+		
+		ogE.addElement(new OptionsGruppeEintrag("Servo-arm", 0));
+		ogE.addElement(new OptionsGruppeEintrag("Conversion beamer", 35));
+		add(o4 = new OptionsUpgradeGruppe(ID, randAbstand, cnt, "", ogE));
+		
+		seperator();
+
+		ogE.addElement(new OptionsGruppeEintrag("Servo-automata", 12));
+		add(o5 = new OptionsZaehlerGruppe(ID, randAbstand, cnt, "", ogE, 4));
+		
+		ogE.addElement(new OptionsGruppeEintrag("Lascutter", 5));
+		ogE.addElement(new OptionsGruppeEintrag("Power fist", 15));
+		ogE.addElement(new OptionsGruppeEintrag("Flamer", 5));
+		ogE.addElement(new OptionsGruppeEintrag("Rotor cannon", 10));
+		add(o6 = new OptionsZaehlerGruppe(ID, randAbstand, cnt, "option", ogE, 4));
+		
+		seperator();
+
+		rkTransport = new RuestkammerStarter(ID, randAbstand, cnt, "ORTransporterKammer2", "Dedicated transport");
+		//Rhino, Pod, Dreadclaw, Phobos, Spartan
+		rkTransport.initKammer(true, false, false, false, false);
+		rkTransport.setButtonText("Dedicated transport");
+		add(rkTransport);
+		
+        sizeSetzen();
+	}
+
+	@Override
+	public void refreshen() {
+		if (!o1.isSelected()) o1.setSelected(0, true);
+		if (!o4.isSelected()) o4.setSelected(0, true);
+				
+		o6.setMaxAnzahl(o5.getAnzahl());
+	}
+        
+}
