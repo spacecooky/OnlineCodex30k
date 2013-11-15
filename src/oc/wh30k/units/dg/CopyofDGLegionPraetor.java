60f182a6d31c710aa12ba71db8d3445c37d2c450
Krimdar <Krimdar@b551ab73-3a01-0010-9fae-47f737f7a8aa>
2013-11-15 05:43:22 +0000

    dg2

---
+++ src/oc/wh30k/units/dg/CopyofDGLegionPraetor.java
@@ -0,0 +1,62 @@
+package oc.wh30k.units.dg;
+
+import oc.BuildaHQ;
+import oc.Eintrag;
+import oc.OptionsGruppeEintrag;
+import oc.OptionsUpgradeGruppe;
+import oc.OptionsZaehlerGruppe;
+import oc.RuestkammerStarter;
+
+public class CopyofDGLegionPraetor extends Eintrag {
+
+	OptionsUpgradeGruppe o0, o1;
+	RuestkammerStarter Servo;
+	RuestkammerStarter Termi;
+
+	public CopyofDGLegionPraetor() {
+		name = "Legion Praetor";
+		grundkosten = 100;
+
+		BuildaHQ.addToInformationVector("DGPraetor", 1);
+		
+		seperator();
+
+		ogE.addElement(new OptionsGruppeEintrag("Terminator armour", 35));
+		ogE.addElement(new OptionsGruppeEintrag("Cataphractii armour", 35));
+		add(o1 = new OptionsUpgradeGruppe(ID, randAbstand, cnt, "", ogE));
+
+		seperator();
+		
+		Servo = new RuestkammerStarter(ID, randAbstand, cnt, "DGPraetorServoKammer", "");
+		Servo.initKammer();
+		Servo.setButtonText(BuildaHQ.translate("Weapons & Equipment"));
+		add(Servo);
+		Servo.setAbwaehlbar(false);
+		
+		seperator();
+		
+		Termi = new RuestkammerStarter(ID, randAbstand, cnt, "DGPraetorTermiKammer", "");
+		Termi.initKammer();
+		Termi.setButtonText(BuildaHQ.translate("Terminatorweapons & Equipment"));
+		add(Termi);
+		Termi.setAbwaehlbar(false);
+		
+		
+		complete();
+	}
+
+	@Override
+	public void deleteYourself() {
+		
+		BuildaHQ.addToInformationVector("DGPraetor", -1);
+		super.deleteYourself();
+	}
+
+	@Override
+	public void refreshen() {
+		Servo.setAktiv(!o1.isSelected());
+		Termi.setAktiv(o1.isSelected());
+		
+	}
+
+}
